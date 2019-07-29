c55586d0342f8f10e7300443db1a7a9b93cf2284
向冰 <872340465@qq.com>
2019-07-29 14:22:54 +0800

    完成专项检查

--- module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
+++ module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
@@ -1,13 +1,12 @@
-package com.zx.module_other.module.print.func.receiver
+package com.zx.module_other.module.print.func.Receiver
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import android.widget.Toast
 import com.frame.zxmvp.baserx.RxManager
+import com.zx.module_other.module.print.bean.PrintBean
 
 class BluetoothReceive : BroadcastReceiver() {
 
@@ -16,24 +15,16 @@ class BluetoothReceive : BroadcastReceiver() {
         // 把搜索的设置添加到集合中
         if (BluetoothDevice.ACTION_FOUND == action) {
             val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-            RxManager().post("Bluetooth", device)
+            //已经匹配的设备
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                //没有匹配的设备
+                RxManager().post("Bluetooth", device)
+            } else {
+                RxManager().post("Bluetooth", device)
+            }
             //搜索完成
         } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
 
-        }else if(BluetoothAdapter.ACTION_STATE_CHANGED == action){
-            val blueState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,0)
-            when(blueState){
-                BluetoothAdapter.STATE_TURNING_ON ->{
-                }
-                BluetoothAdapter.STATE_ON ->{
-                    RxManager().post("bluetoothOpen",BluetoothAdapter.STATE_ON)
-                }
-                BluetoothAdapter.STATE_TURNING_OFF->{
-                }
-                BluetoothAdapter.STATE_OFF ->{
-                    RxManager().post("bluetoothOpen",BluetoothAdapter.STATE_OFF)
-                }
-            }
         }
     }
 
