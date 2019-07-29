49e0c812d49cbb525a044b24b20f6f6e159f5b7f
向冰 <872340465@qq.com>
2019-07-29 15:57:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt

--- module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
+++ module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
@@ -5,8 +5,6 @@ import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import android.widget.Toast
 import com.frame.zxmvp.baserx.RxManager
 
 class BluetoothReceive : BroadcastReceiver() {
@@ -16,7 +14,13 @@ class BluetoothReceive : BroadcastReceiver() {
         // 把搜索的设置添加到集合中
         if (BluetoothDevice.ACTION_FOUND == action) {
             val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-            RxManager().post("Bluetooth", device)
+            //已经匹配的设备
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                //没有匹配的设备
+                RxManager().post("Bluetooth", device)
+            } else {
+                RxManager().post("Bluetooth", device)
+            }
             //搜索完成
         } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
 
