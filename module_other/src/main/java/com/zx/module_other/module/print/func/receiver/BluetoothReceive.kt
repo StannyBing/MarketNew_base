caa50e56bc5df5e0bd9d7852eff39c4df119f43e
liyuanchao <liyuanchao@smartspyeye.com>
2019-07-29 09:28:53 +0800

    1.冲突问题解决

--- module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
+++ module_other/src/main/java/com/zx/module_other/module/print/func/receiver/BluetoothReceive.kt
@@ -20,19 +20,27 @@ class BluetoothReceive : BroadcastReceiver() {
             //搜索完成
         } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action) {
 
-        }else if(BluetoothAdapter.ACTION_STATE_CHANGED == action){
-            val blueState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,0)
-            when(blueState){
-                BluetoothAdapter.STATE_TURNING_ON ->{
+        } else if (BluetoothAdapter.ACTION_STATE_CHANGED == action) {
+            val blueState = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, 0)
+            when (blueState) {
+                BluetoothAdapter.STATE_TURNING_ON -> {
                 }
-                BluetoothAdapter.STATE_ON ->{
-                    RxManager().post("bluetoothOpen",BluetoothAdapter.STATE_ON)
+                BluetoothAdapter.STATE_ON -> {
+                    RxManager().post("bluetoothOpen", BluetoothAdapter.STATE_ON)
                 }
-                BluetoothAdapter.STATE_TURNING_OFF->{
+                BluetoothAdapter.STATE_TURNING_OFF -> {
                 }
-                BluetoothAdapter.STATE_OFF ->{
-                    RxManager().post("bluetoothOpen",BluetoothAdapter.STATE_OFF)
+                BluetoothAdapter.STATE_OFF -> {
+                    RxManager().post("bluetoothOpen", BluetoothAdapter.STATE_OFF)
                 }
+            }
+        } else if (BluetoothDevice.ACTION_BOND_STATE_CHANGED == action) {
+            val state = intent.getIntExtra(BluetoothDevice.EXTRA_BOND_STATE, -1)
+            val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+            when (state) {
+                BluetoothDevice.BOND_NONE -> RxManager().post("bluetoothState", device)
+                BluetoothDevice.BOND_BONDING -> RxManager().post("bluetoothState", device)
+                BluetoothDevice.BOND_BONDED -> RxManager().post("bluetoothState", device)
             }
         }
     }
